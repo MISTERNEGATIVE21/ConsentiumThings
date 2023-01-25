c0b5d7d4a0dde0d97772656fa330aed15ae2fdc4
Debjyoti <consentium.inc@gmail.com>
2023-01-25 14:11:37 +0530

    updated Network LED pin

--- src/ConsentiumThings.h
+++ src/ConsentiumThings.h
@@ -6,17 +6,18 @@
 #ifdef ESP8266
     #include <ESP8266WiFi.h>
     #include <ESP8266HTTPClient.h>
-    #include <WiFiClient.h>        
+    #include <WiFiClient.h> 
+    #define NETWRK_LED 16       
 #endif
 #ifdef ESP32
     #include <WiFi.h>
     #include <HTTPClient.h>  
+    #define NETWRK_LED 2
 #endif
 
 #define THRES_5 4.8828
 #define THRES_3 3.2226
 #define EspBaud 115200
-#define NETWRK_LED 16
 
 #define LOW_PRE 2
 #define MID_PRE 4
